bec298966622b5af2bb6f6948674283b701e589a
hzmangel <hzmangel@gmail.com>
2012-06-18 11:09:12 +0800

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheaterRecord::Application.config.secret_token = 'd94a541117f7d36fcc7e1c1b7393bf08f46dd08e33a99ea9adfae6b65835318ba88db4a918732f7d2596e2227152450d0c9ebfa726faab7053c43e771edf8dfe'
